85e07066ea8a2e1c7e7621e40502686e7e3ac34c
Chandramouli Maddala <chandramouli@netskope.com>
2020-05-27 10:33:53 +0530

    get ChnageLog for Pipeline jobs

--- src/main/java/jenkinsci/plugins/influxdb/generators/ChangeLogPointGenerator.java
+++ src/main/java/jenkinsci/plugins/influxdb/generators/ChangeLogPointGenerator.java
@@ -1,6 +1,9 @@
 package jenkinsci.plugins.influxdb.generators;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.influxdb.dto.Point;
 
@@ -12,6 +15,7 @@ import jenkinsci.plugins.influxdb.renderer.MeasurementRenderer;
 public class ChangeLogPointGenerator extends AbstractPointGenerator {
     
 	private static final String BUILD_DISPLAY_NAME = "display_name";
+	private static final Logger logger = Logger.getLogger(ChangeLogPointGenerator.class.getName());
 
 	private final Run<?, ?> build;
 	private final String customPrefix;
@@ -32,6 +36,33 @@ public class ChangeLogPointGenerator extends AbstractPointGenerator {
 	}
 
 	public boolean hasReport() {
+		if(build.getClass().getName().equals("org.jenkinsci.plugins.workflow.job.WorkflowRun")) {
+			org.jenkinsci.plugins.workflow.job.WorkflowRun workflowRun = (org.jenkinsci.plugins.workflow.job.WorkflowRun) build;
+			this.affectedPaths = new StringBuilder();
+
+			this.messages = new StringBuilder();
+
+			this.culprits = new StringBuilder();
+			List<ChangeLogSet<? extends ChangeLogSet.Entry>> changesets = workflowRun.getChangeSets();
+			for (ChangeLogSet<? extends ChangeLogSet.Entry> changeset : changesets ) {
+				for (ChangeLogSet.Entry str : changeset) {
+					Collection<? extends ChangeLogSet.AffectedFile> affectedFiles = str.getAffectedFiles();
+					for (ChangeLogSet.AffectedFile affectedFile : affectedFiles) {
+						this.affectedPaths.append(affectedFile.getPath());
+						this.affectedPaths.append(", ");
+					}
+					this.messages.append(str.getMsg());
+					this.messages.append(", ");
+
+					this.culprits.append(str.getAuthor().getFullName());
+					this.culprits.append(", ");
+
+					this.commitCount += 1;
+				}
+			}
+			logger.log(Level.INFO, "hasReport Commit Count:: "+ this.getCommitCount());
+            return (this.getCommitCount() > 0);
+        }
 		if (build instanceof AbstractBuild) {
 			getChangeLog(build);
 			return (this.getCommitCount() > 0);
